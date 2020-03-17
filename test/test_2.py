44ebc032851ab4b6df42099d7087e97122adf128
Umeshkumar B Koli <42834215+UmeshkumarKoli@users.noreply.github.com>
2020-03-17 14:13:02 +0530

    Update test_2.py

--- test/test_2.py
+++ test/test_2.py
@@ -8,7 +8,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.mark.usefixtures("setup") 
 class Test2():
-    
+    @pytest.mark.usefixtures("resource_1_teardown") 
     def test_2(self):      
         wait=WebDriverWait(self.driver,10)
         self.driver.get('http://www.google.co.in')
+        print "test2
